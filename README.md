407c81b9f85d10e5a937da1c2ef1d5c2640c76bc
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-15 16:27:45 +0530

    update readme

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # JellyExplorer plugin
 
+Run the following command to build and install the plugin
+
 ```
-clear && 
-dotnet build &&
-scp .\Jellyfin.Plugin.JellyExplorer\bin\Debug\net6.0\Jellyfin.Plugin.JellyExplorer.dll 'mediaserver@192.168.1.6:/tmp/jellyfin-plugins/' &&
-ssh -t mediaserver@192.168.1.6 'cd /var/lib/jellyfin/plugins/local-plugin/ && sudo mv /tmp/jellyfin-plugins/*.dll . && sudo chown jellyfin:jellyfin ./*.dll && sudo service jellyfin restart && sudo service jellyfin status | grep Active:'
+.\reinstallPlugin.bat
 ```
-
-
-
-                            
--- reinstallPlugin.bat
+++ reinstallPlugin.bat
@@ -1,11 +1,11 @@
-set on_remote=ssh -t mediaserver@192.168.1.10
+set on_remote=ssh -t mediaserver@192.168.1.16
 
 dotnet build 
 
 %on_remote% "mkdir -p /tmp/jellyfin-plugins/"
 %on_remote% "mkdir -p /var/lib/jellyfin/plugins/local-plugin/"
 
-scp -r .\Jellyfin.Plugin.JellyExplorer\bin\Debug\net6.0\Jellyfin.Plugin.JellyExplorer.dll mediaserver@192.168.1.10:/tmp/jellyfin-plugins/
+scp -r .\Jellyfin.Plugin.JellyExplorer\bin\Debug\net6.0\Jellyfin.Plugin.JellyExplorer.dll mediaserver@192.168.1.6:/tmp/jellyfin-plugins/
 
 %on_remote% "sudo install -d -m 0755 -o jellyfin -g jellyfin ./local-plugin/"
 %on_remote% "sudo mv /tmp/jellyfin-plugins/*.dll /var/lib/jellyfin/plugins/local-plugin/"
